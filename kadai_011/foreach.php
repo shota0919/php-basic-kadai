f0d327be417778ba5d203b990c0123fbd2d81bb1
shota0919 <131502448+shota0919@users.noreply.github.com>
2023-10-14 09:01:03 +0900

    Add files via upload

---
+++ kadai_011/foreach.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+ <html lang="ja">
+ 
+ <head>
+     <meta charset="UTF-8">
+     <title>PHP基礎編</title>
+ </head>
+ 
+ <body>
+     <p>
+         <?php
+        $vegetables_data = ['名前' => '玉ねぎ', '値段' => 200, '産地' => '北海道'];
+ 
+         //  配列$user_namesの要素を1つずつ順番に出力する
+         foreach ($vegetables_data as $key => $value) {
+            echo "{$key}:{$value}<br>";
+        }
+         ?>
+     </p>
+
+ </body>
+ 
+ </html>
